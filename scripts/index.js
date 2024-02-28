97c7c3d5161bff8118ebbbdf15cec917a682f16f
Jisun696 <istiakkashem35@gmail.com>
2024-02-29 02:27:15 +0600

    Added all the dynamic products
    added codes to dynamically fetch data through given api and displayed them in the UI

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,31 @@
+const productsContainer = document.getElementById('products__container');
+
 const loadPhones = async () => {
   const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
   const data = await res.json();
   const phones = data.data;
-  for (let phone of phones) {
-    console.log(phone.slug);
-  }
+  displayPhone(phones);
+};
+
+const displayPhone = (phones) => {
+  phones.forEach((phone) => {
+    productsContainer.innerHTML += `
+    <div
+      class="flex flex-col items-center justify-center rounded-lg border border-['#CFCFCF'] px-9 py-6">
+      <div class="w-full h-[300px] flex items-center justify-center bg-blue-600 bg-opacity-5 rounded-lg mb-6">
+        <img src=${phone.image} class="" alt="" />
+      </div>
+      <h3 class="text-neutral-700 text-[25px] font-bold mb-5">${phone.phone_name}</h3>
+      <p class="text-neutral-500 text-lg font-normal text-center mb-2">
+        There are many variations of passages of available, but the majority have suffered
+      </p>
+      <p class="text-neutral-700 text-[25px] font-bold mb-4">$999</p>
+      <button class="bg-blue-600 rounded-lg py-2 px-6 text-white text-xl font-semibold">
+        Show Details
+      </button>
+    </div>
+    `;
+  });
 };
 
 loadPhones();
