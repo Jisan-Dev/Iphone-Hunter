417503fc2b71706b818f167c9d66d5043a4b44f0
Jisun696 <istiakkashem35@gmail.com>
2024-02-29 20:36:26 +0600

    Add logic to display msg when there's no data available

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 const productsContainer = document.getElementById('products__container');
+const nodataContainer = document.getElementById('nodata_container');
 const searchInp = document.getElementById('searchInp');
 const showAllBtn = document.getElementById('products__btn-show-all');
 
@@ -10,26 +11,33 @@ const loadPhones = async (searchValue = '13', isShowAll) => {
 };
 
 const displayPhone = (phones, isShowAll) => {
-  // make container empty every time new search hit
-  productsContainer.innerHTML = '';
-
-  // display 'show all' btn if products are more than 12 in quantity
-  if (phones.length > 12 && !isShowAll) {
-    showAllBtn.classList.remove('hidden');
+  if (phones.length === 0) {
+    nodataContainer.innerHTML = `<h4 class="text-3xl font-extrabold text-center">No data available</h4>`;
+    productsContainer.innerHTML = '';
+    console.log('nai');
   } else {
-    showAllBtn.classList.add('hidden');
-  }
+    nodataContainer.innerHTML = '';
 
-  // limiting to only show first 12 results
-  if (!isShowAll) {
-    phones = phones.slice(0, 12);
-  }
+    // make container empty every time new search hit
+    productsContainer.innerHTML = '';
 
-  // loop through each phone object and insert html
-  phones.forEach((phone) => {
-    productsContainer.innerHTML += `
+    // display 'show all' btn if products are more than 12 in quantity
+    if (phones.length > 12 && !isShowAll) {
+      showAllBtn.classList.remove('hidden');
+    } else {
+      showAllBtn.classList.add('hidden');
+    }
+
+    // limiting to only show first 12 results
+    if (!isShowAll) {
+      phones = phones.slice(0, 12);
+    }
+
+    // loop through each phone object and insert html
+    phones.forEach((phone) => {
+      productsContainer.innerHTML += `
     <div
-      class="flex flex-col items-center justify-center rounded-lg border border-['#CFCFCF'] px-9 py-6 shadow-lg">
+      class="flex flex-col items-center justify-center rounded-lg border border-['#CFCFCF'] px-9 py-6 shadow-lg -mt-2">
       <div class="w-full h-[300px] flex items-center justify-center bg-blue-600 bg-opacity-5 rounded-lg mb-6">
         <img src=${phone.image} class="" alt="" />
       </div>
@@ -43,7 +51,8 @@ const displayPhone = (phones, isShowAll) => {
       </button>
     </div>
     `;
-  });
+    });
+  }
   handleLoadingSpinner(false);
 };
 
